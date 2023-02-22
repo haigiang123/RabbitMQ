70e4c592d0b9065012dac4c14275a4c949ee4fd3
thle <thle@nthrive.com>
2023-02-22 16:10:50 +0700

    Init RabitMQ project

---
+++ Training_RabbitMQ/NewTask/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace NewTask
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            NewTaskMessage();
+        }
+
+        public static void NewTaskMessage()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "competing_consumer_pattern", durable: false, exclusive: false, autoDelete: false, null);
+
+            List<string> messages = new List<string>()
+            {
+                "message 1",
+                "message 2",
+                "message 3",
+                "message 4",
+                "message 5",
+                "message 6",
+                "message 7",
+                "message 8",
+            };
+
+            foreach (string message in messages)
+            {
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: string.Empty, routingKey: "competing_consumer_pattern", null,
+                    body: body);
+                Console.WriteLine($" [x] Sent {message}");
+
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Training_RabbitMQ/Worker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker();
+        }
+
+        public static void Worker()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "competing_consumer_pattern", durable: false, exclusive: false, autoDelete: false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (a, data) =>
+            {
+                var message = Encoding.UTF8.GetString(data.Body.ToArray());
+
+                Console.WriteLine($" [x] Received {message}");
+
+                Thread.Sleep(2000);
+
+                //channel.BasicAck(deliveryTag: data.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(queue: "competing_consumer_pattern", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+    }
+}
