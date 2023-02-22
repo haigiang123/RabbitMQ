67319e057e78bd390479c820029a6be7f0c7db45
thle <thle@nthrive.com>
2023-02-22 16:30:19 +0700

    - Our queue/message is still alive thought rabbitmq server is restarted/stop - We can use Confirm for this issue to get better performance and experience

--- Training_RabbitMQ/Worker/Program.cs
+++ Training_RabbitMQ/Worker/Program.cs
@@ -18,7 +18,7 @@ namespace Worker
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
-            channel.QueueDeclare(queue: "competing_consumer_pattern", durable: false, exclusive: false, autoDelete: false, null);
+            channel.QueueDeclare(queue: "competing_consumer_pattern", durable: true, exclusive: false, autoDelete: false, null);
 
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (a, data) =>
